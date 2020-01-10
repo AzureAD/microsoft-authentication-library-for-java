5973fba1a3c04f40d8387f919473d26b31d2c8b6
Petro <petro@Petros-MacBook-Pro.local>
2020-01-09 17:52:15 -0800

    jackson config switch to older api

--- src/main/java/com/microsoft/aad/msal4j/JsonHelper.java
+++ src/main/java/com/microsoft/aad/msal4j/JsonHelper.java
@@ -4,19 +4,23 @@
 package com.microsoft.aad.msal4j;
 
 import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.core.JsonParser;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.core.json.JsonReadFeature;
 import com.fasterxml.jackson.databind.DeserializationFeature;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.SerializationFeature;
 import com.fasterxml.jackson.databind.json.JsonMapper;
 
 class JsonHelper {
+    static ObjectMapper mapper;
 
-    static ObjectMapper mapper = JsonMapper.builder()
-            .configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false)
-            .configure(JsonReadFeature.ALLOW_UNESCAPED_CONTROL_CHARS, true)
-            .serializationInclusion(JsonInclude.Include.NON_NULL)
-            .build();
+    static {
+        mapper = new ObjectMapper();
+        mapper.configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false);
+        mapper.configure(JsonParser.Feature.ALLOW_UNQUOTED_CONTROL_CHARS, true);
+        mapper.setSerializationInclusion(JsonInclude.Include.NON_NULL);
+    }
 
     static <T> T convertJsonToObject(final String json, final Class<T> clazz) {
         try {
