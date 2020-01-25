319828fef6ca28d99c43da972c636595f89d34d4
Petro <petro@Petros-MacBook-Pro.local>
2020-01-24 20:36:58 -0800

    same site fix

--- src/samples/spring-security-web-app/src/main/java/com/microsoft/azure/springsecuritywebapp/AppConfiguration.java
+++ src/samples/spring-security-web-app/src/main/java/com/microsoft/azure/springsecuritywebapp/AppConfiguration.java
@@ -3,11 +3,15 @@
 
 package com.microsoft.azure.springsecuritywebapp;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.security.oauth2.client.EnableOAuth2Sso;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.annotation.Order;
+import org.springframework.core.env.Environment;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+import java.net.URLEncoder;
 
 
 @Configuration
@@ -15,8 +19,14 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
 @Order(value = 0)
 public class AppConfiguration extends WebSecurityConfigurerAdapter {
 
+    @Autowired
+    private Environment env;
+
     @Override
     public void configure(HttpSecurity http) throws Exception {
+
+        String logoutUrl = env.getProperty("endSessionEndpoint") + "?post_logout_redirect_uri=" +
+                URLEncoder.encode(env.getProperty("homePage"), "UTF-8");
 
         http.antMatcher("/**")
                 .authorizeRequests()
@@ -28,6 +38,6 @@ public class AppConfiguration extends WebSecurityConfigurerAdapter {
                     .logout()
                         .deleteCookies()
                         .invalidateHttpSession(true)
-                        .logoutSuccessUrl("/");
+                        .logoutSuccessUrl(logoutUrl);
     }
 }
