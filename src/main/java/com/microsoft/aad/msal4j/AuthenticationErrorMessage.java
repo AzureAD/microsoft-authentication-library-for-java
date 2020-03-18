9c84702bc9c613571f3a28569dfc3a0317e08aa2
Avery-Dunn <avdunn@microsoft.com>
2020-03-18 10:48:22 -0700

    Minor typo fix

--- src/main/java/com/microsoft/aad/msal4j/AuthenticationErrorMessage.java
+++ src/main/java/com/microsoft/aad/msal4j/AuthenticationErrorMessage.java
@@ -6,7 +6,7 @@ package com.microsoft.aad.msal4j;
 public class AuthenticationErrorMessage {
 
     /**
-     * Token not found it the cache
+     * Token not found in the cache
      */
-    public final static String NO_TOKEN_IN_CACHE = "Token not found it the cache";
+    public final static String NO_TOKEN_IN_CACHE = "Token not found in the cache";
 }
