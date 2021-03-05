67e5567c4ac6911b6ebe45fe329cb9e9da785582
SomkaPe <pesomka@microsoft.com>
2021-03-04 17:03:38 -0800

    Pesomka/b2b stage1 (#356)
    multi cloud accounts

--- src/test/java/com/microsoft/aad/msal4j/AccountTest.java
+++ src/test/java/com/microsoft/aad/msal4j/AccountTest.java
@@ -7,8 +7,12 @@ import org.testng.Assert;
 import org.testng.annotations.Test;
 import java.io.IOException;
 import java.net.URISyntaxException;
+import java.util.Base64;
 import java.util.Iterator;
 import java.util.Map;
+import java.util.Set;
+
+import static com.microsoft.aad.msal4j.Constants.POINT_DELIMITER;
 
 public class AccountTest {
 
@@ -64,5 +68,95 @@ public class AccountTest {
                 }
             }
         }
+    }
+
+
+    String getEmptyBase64EncodedJson(){
+        return new String(Base64.getEncoder().encode("{}".getBytes()));
+    }
+
+    String getJWTHeaderBase64EncodedJson(){
+        return new String(Base64.getEncoder().encode("{\"alg\": \"HS256\", \"typ\": \"JWT\"}".getBytes()));
+    }
+
+    private String getTestIdToken(String environment, String tenant) throws IOException, URISyntaxException {
+        String claims = "{\n" +
+                "  \"iss\": \"" + environment + "\",\n" +
+                "  \"tid\": \"" + tenant + "\"\n" +
+                "}";
+
+        String encodedIdToken = new String(Base64.getEncoder().encode(claims.getBytes()), "UTF-8");
+
+        encodedIdToken = getJWTHeaderBase64EncodedJson() + POINT_DELIMITER +
+                encodedIdToken + POINT_DELIMITER +
+                getEmptyBase64EncodedJson();
+
+        return encodedIdToken;
+    }
+
+    @Test
+    public void multiCloudAccount_aggregatedInGetAccountsRemoveAccountApis() throws IOException, URISyntaxException {
+        String BLACK_FORESRT_TENANT = "de_tid";
+        String WW_TENTANT = "tid";
+        String BLACK_FOREST_ENV = "login.microsoftonline.de";
+        String WW_ENV = "login.microsoftonline.com";
+        String CLIENT_ID = "client_id";
+        String DE_ID_TOKEN_PLACEHOLDER = "<de_id_token_placeholder>";
+        String ID_TOKEN_PLACEHOLDER = "<id_token_placeholder>";
+
+        String cacheWithMultiCloudAccount = TestHelper.readResource(
+                this.getClass(), "/cache_data/multi-cloud-account-cache.json");
+
+        cacheWithMultiCloudAccount = cacheWithMultiCloudAccount.replace
+                (DE_ID_TOKEN_PLACEHOLDER, getTestIdToken(BLACK_FOREST_ENV, BLACK_FORESRT_TENANT));
+
+        cacheWithMultiCloudAccount = cacheWithMultiCloudAccount.replace
+                (ID_TOKEN_PLACEHOLDER, getTestIdToken(WW_ENV, WW_TENTANT));
+
+        ITokenCacheAccessAspect persistenceAspect = new ITokenCacheAccessAspect() {
+            String data;
+
+            @Override
+            public void beforeCacheAccess(ITokenCacheAccessContext iTokenCacheAccessContext) {
+                iTokenCacheAccessContext.tokenCache().deserialize(data);
+            }
+
+            @Override
+            public void afterCacheAccess(ITokenCacheAccessContext iTokenCacheAccessContext) {
+                data = iTokenCacheAccessContext.tokenCache().serialize();
+            }
+
+            ITokenCacheAccessAspect init(String data){
+                this.data = data;
+                return this;
+            }
+        }.init(cacheWithMultiCloudAccount);
+
+        // acquire tokens for home tenant, and serialize cache
+        PublicClientApplication pca = PublicClientApplication.builder(
+                CLIENT_ID).
+                authority(TestConstants.COMMON_AUTHORITY)
+                .setTokenCacheAccessAspect(persistenceAspect)
+                .build();
+
+        Set<IAccount> accounts = pca.getAccounts().join();
+
+        Assert.assertEquals(accounts.size(), 1);
+        IAccount account = accounts.iterator().next();
+
+        Map<String, ITenantProfile> tenantProfiles = account.getTenantProfiles();
+        Assert.assertEquals(tenantProfiles.size(), 2);
+
+        Assert.assertTrue(tenantProfiles.containsKey(BLACK_FORESRT_TENANT));
+        Assert.assertTrue(tenantProfiles.containsKey(WW_TENTANT));
+
+        pca.removeAccount(account).join();
+        accounts = pca.getAccounts().join();
+        Assert.assertEquals(accounts.size(), 0);
+
+        Assert.assertEquals(pca.tokenCache.accounts.size(), 0);
+        Assert.assertEquals(pca.tokenCache.idTokens.size(), 0);
+        Assert.assertEquals(pca.tokenCache.refreshTokens.size(), 0);
+        Assert.assertEquals(pca.tokenCache.accessTokens.size(), 0);
     }
 }
