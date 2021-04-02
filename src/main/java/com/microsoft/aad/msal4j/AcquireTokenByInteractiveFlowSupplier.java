84729c08dc5ffc1991ac124499322f3e2570d212
Avery-Dunn <62066438+Avery-Dunn@users.noreply.github.com>
2021-04-01 19:41:15 -0700

    Instance aware support (#362)
     Instance aware support for interactive requests

--- src/main/java/com/microsoft/aad/msal4j/AcquireTokenByInteractiveFlowSupplier.java
+++ src/main/java/com/microsoft/aad/msal4j/AcquireTokenByInteractiveFlowSupplier.java
@@ -157,11 +157,24 @@ class AcquireTokenByInteractiveFlowSupplier extends AuthenticationResultSupplier
                 clientApplication,
                 clientApplication.createRequestContext(PublicApi.ACQUIRE_TOKEN_BY_AUTHORIZATION_CODE, parameters));
 
+        Authority authority;
+
+        //The result field of an AuthorizationResult object is only set if the response contained the 'cloud_instance_host_name' key,
+        // which indicates that this token request is instance aware and should use that as the environment value
+        //Otherwise, use the authority value from the client application
+        if (authorizationResult.environment() != null) {
+            authority = Authority.createAuthority(new URL(clientApplication.authenticationAuthority.canonicalAuthorityUrl.getProtocol(),
+                    authorizationResult.environment(),
+                    clientApplication.authenticationAuthority.canonicalAuthorityUrl.getFile()));
+        } else {
+            authority = clientApplication.authenticationAuthority;
+        }
+
         AcquireTokenByAuthorizationGrantSupplier acquireTokenByAuthorizationGrantSupplier =
             new AcquireTokenByAuthorizationGrantSupplier(
                     clientApplication,
                     authCodeRequest,
-                    clientApplication.authenticationAuthority);
+                    authority);
 
         return acquireTokenByAuthorizationGrantSupplier.execute();
     }
