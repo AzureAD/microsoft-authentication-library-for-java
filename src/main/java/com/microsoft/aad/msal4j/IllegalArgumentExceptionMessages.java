92cc182e26d9ecbd76aee8f76823aa355b82fa0e
Matt Mazzola <mattm@microsoft.com>
2022-04-25 10:50:49 -0700

    Update IllegalArgumentExceptionMessages.java
    fix typo

--- src/main/java/com/microsoft/aad/msal4j/IllegalArgumentExceptionMessages.java
+++ src/main/java/com/microsoft/aad/msal4j/IllegalArgumentExceptionMessages.java
@@ -7,7 +7,7 @@ class IllegalArgumentExceptionMessages {
 
     final static String AUTHORITY_URI_EMPTY_PATH_SEGMENT = "Authority Uri should not have empty path segments";
 
-    final static String AUTHORITY_URI_MISSING_PATH_SEGMENT = "Authority Uri must have a least one path segment. This is usually 'common' or the application's tenant id.";
+    final static String AUTHORITY_URI_MISSING_PATH_SEGMENT = "Authority Uri must have at least one path segment. This is usually 'common' or the application's tenant id.";
 
     final static String AUTHORITY_URI_EMPTY_PATH = "Authority Uri should have at least one segment in the path";
 
