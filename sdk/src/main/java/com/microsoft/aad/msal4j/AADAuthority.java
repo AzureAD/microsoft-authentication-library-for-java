5d5825daeafd6b0e7701ebba4570abb804ccf75a
siddhijain <siddhijain@microsoft.com>
2022-09-02 17:30:45 -0500

    Initial commit

--- sdk/src/main/java/com/microsoft/aad/msal4j/AADAuthority.java
+++ sdk/src/main/java/com/microsoft/aad/msal4j/AADAuthority.java
@@ -13,15 +13,15 @@ import lombok.experimental.Accessors;
 @Getter(AccessLevel.PACKAGE)
 class AADAuthority extends Authority {
 
-    private final static String TENANTLESS_TENANT_NAME = "common";
-    private final static String AUTHORIZATION_ENDPOINT = "oauth2/v2.0/authorize";
-    private final static String TOKEN_ENDPOINT = "oauth2/v2.0/token";
-    final static String DEVICE_CODE_ENDPOINT = "oauth2/v2.0/devicecode";
+    private static final String TENANTLESS_TENANT_NAME = "common";
+    private static final String AUTHORIZATION_ENDPOINT = "oauth2/v2.0/authorize";
+    private static final String TOKEN_ENDPOINT = "oauth2/v2.0/token";
+    static final String DEVICE_CODE_ENDPOINT = "oauth2/v2.0/devicecode";
 
-    private final static String AAD_AUTHORITY_FORMAT = "https://%s/%s/";
-    private final static String AAD_AUTHORIZATION_ENDPOINT_FORMAT = AAD_AUTHORITY_FORMAT + AUTHORIZATION_ENDPOINT;
-    private final static String AAD_TOKEN_ENDPOINT_FORMAT = AAD_AUTHORITY_FORMAT + TOKEN_ENDPOINT;
-    private final static String DEVICE_CODE_ENDPOINT_FORMAT = AAD_AUTHORITY_FORMAT + DEVICE_CODE_ENDPOINT;
+    private static final String AAD_AUTHORITY_FORMAT = "https://%s/%s/";
+    private static final String AAD_AUTHORIZATION_ENDPOINT_FORMAT = AAD_AUTHORITY_FORMAT + AUTHORIZATION_ENDPOINT;
+    private static final String AAD_TOKEN_ENDPOINT_FORMAT = AAD_AUTHORITY_FORMAT + TOKEN_ENDPOINT;
+    private static final String DEVICE_CODE_ENDPOINT_FORMAT = AAD_AUTHORITY_FORMAT + DEVICE_CODE_ENDPOINT;
 
     AADAuthority(final URL authorityUrl) {
         super(authorityUrl, AuthorityType.AAD);
