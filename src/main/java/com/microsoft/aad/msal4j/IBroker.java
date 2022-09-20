0c7633f12cc78b7907f74ef494ca5a26d27151b6
Avery-Dunn <avdunn@microsoft.com>
2022-09-20 09:30:10 -0700

    Add defaults to IBroker

--- src/main/java/com/microsoft/aad/msal4j/IBroker.java
+++ src/main/java/com/microsoft/aad/msal4j/IBroker.java
@@ -19,7 +19,9 @@ public interface IBroker {
      * Will also detect dependency or accessibility errors with the Broker package, allowing an exception to provide the error to the app developer
      * before any actual calls to a broker are made
      */
-    void initializeBroker();
+    default void initializeBroker(){
+        throw new MsalClientException("Broker implementation missing", AuthenticationErrorCode.MISSING_BROKER);
+    }
 
     /**
      * Acquire a token silently, i.e. without direct user interaction
@@ -27,10 +29,12 @@ public interface IBroker {
      * This may be accomplished by returning tokens from a token cache, using cached refresh tokens to get new tokens,
      * or via any authentication flow where a user is not prompted to enter credentials
      *
-     * @param request MsalRequest object which contains everything needed for the broker implementation to make a request
+     * @param requestParameters MsalRequest object which contains everything needed for the broker implementation to make a request
      * @return IBroker implementations will return an AuthenticationResult object
      */
-    AuthenticationResult acquireToken(SilentRequest request);
+    default IAuthenticationResult acquireToken(PublicClientApplication application, SilentParameters requestParameters) {
+        throw new MsalClientException("Broker implementation missing", AuthenticationErrorCode.MISSING_BROKER);
+    }
 
     /**
      * Acquire a token silently, i.e. without direct user interaction, using a refresh token
@@ -38,34 +42,44 @@ public interface IBroker {
      * This requires the broker implementation to cache the refresh tokens, either in its own cache or by populating MSAL Java's cache,
      * or otherwise have some way of retrieving refresh tokens cached somewhere else
      *
-     * @param request MsalRequest object which contains everything needed for the broker implementation to make a request
+     * @param requestParameters MsalRequest object which contains everything needed for the broker implementation to make a request
      * @return IBroker implementations will return an AuthenticationResult object
      */
-    AuthenticationResult acquireToken(RefreshTokenRequest request);
+    default IAuthenticationResult acquireToken(PublicClientApplication application, RefreshTokenParameters requestParameters) {
+        throw new MsalClientException("Broker implementation missing", AuthenticationErrorCode.MISSING_BROKER);
+    }
 
     /**
      * Acquire a token interactively, by prompting users to enter their credentials in some way
      *
-     * @param request MsalRequest object which contains everything needed for the broker implementation to make a request
+     * @param requestParameters MsalRequest object which contains everything needed for the broker implementation to make a request
      * @return IBroker implementations will return an AuthenticationResult object
      */
-    AuthenticationResult acquireToken(InteractiveRequest request);
+    default IAuthenticationResult acquireToken(PublicClientApplication application, InteractiveRequestParameters requestParameters) {
+        throw new MsalClientException("Broker implementation missing", AuthenticationErrorCode.MISSING_BROKER);
+    }
 
     /**
      * Acquire a token silently, i.e. without direct user interaction, using username/password authentication
      *
-     * @param request MsalRequest object which contains everything needed for the broker implementation to make a request
+     * @param requestParameters MsalRequest object which contains everything needed for the broker implementation to make a request
      * @return IBroker implementations will return an AuthenticationResult object
      */
-    AuthenticationResult acquireToken(UserNamePasswordParameters request);
+    default IAuthenticationResult acquireToken(PublicClientApplication application, UserNamePasswordParameters requestParameters) {
+        throw new MsalClientException("Broker implementation missing", AuthenticationErrorCode.MISSING_BROKER);
+    }
 
     /**
      * Retrieve an account information from the broker based on an account ID
      *
      * @return IAccount implementations will return an Account object
      */
-    Account getAccount(String id);
+    default Account getAccount(String id) {
+        throw new MsalClientException("Broker implementation missing", AuthenticationErrorCode.MISSING_BROKER);
+    }
 
     //TODO: Not needed? ClientApplication has getAccounts(), but that would rely on us caching the accounts, which MSALRuntime also caches
-    Set<Account> getAccounts();
+    default Set<Account> getAccounts() {
+        throw new MsalClientException("Broker implementation missing", AuthenticationErrorCode.MISSING_BROKER);
+    }
 }
