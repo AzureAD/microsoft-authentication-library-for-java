6c4897a93d5b618fa1f422484a8c425e13b1ebfb
siddhijain <siddhijain@microsoft.com>
2022-09-27 09:44:52 -0500

    address PR comments

--- msal4j-sdk/src/main/java/com/microsoft/aad/msal4j/IBroker.java
+++ msal4j-sdk/src/main/java/com/microsoft/aad/msal4j/IBroker.java
@@ -4,6 +4,7 @@
 package com.microsoft.aad.msal4j;
 
 import java.util.Set;
+import java.util.concurrent.CompletableFuture;
 
 /**
  * Used to define the basic set of methods that all Brokers must implement
@@ -37,19 +38,6 @@ public interface IBroker {
     }
 
     /**
-     * Acquire a token silently, i.e. without direct user interaction, using a refresh token
-     *
-     * This requires the broker implementation to cache the refresh tokens, either in its own cache or by populating MSAL Java's cache,
-     * or otherwise have some way of retrieving refresh tokens cached somewhere else
-     *
-     * @param requestParameters MsalRequest object which contains everything needed for the broker implementation to make a request
-     * @return IBroker implementations will return an AuthenticationResult object
-     */
-    default IAuthenticationResult acquireToken(PublicClientApplication application, RefreshTokenParameters requestParameters) {
-        throw new MsalClientException("Broker implementation missing", AuthenticationErrorCode.MISSING_BROKER);
-    }
-
-    /**
      * Acquire a token interactively, by prompting users to enter their credentials in some way
      *
      * @param requestParameters MsalRequest object which contains everything needed for the broker implementation to make a request
@@ -69,17 +57,7 @@ public interface IBroker {
         throw new MsalClientException("Broker implementation missing", AuthenticationErrorCode.MISSING_BROKER);
     }
 
-    /**
-     * Retrieve an account information from the broker based on an account ID
-     *
-     * @return IAccount implementations will return an Account object
-     */
-    default Account getAccount(String id) {
-        throw new MsalClientException("Broker implementation missing", AuthenticationErrorCode.MISSING_BROKER);
-    }
-
-    //TODO: Not needed? ClientApplication has getAccounts(), but that would rely on us caching the accounts, which MSALRuntime also caches
-    default Set<Account> getAccounts() {
+    default CompletableFuture removeAccount(IAccount account) {
         throw new MsalClientException("Broker implementation missing", AuthenticationErrorCode.MISSING_BROKER);
     }
 }
