28cfa56d7ac10e34dac6b41731223e50f36b62b9
Avery-Dunn <62066438+Avery-Dunn@users.noreply.github.com>
2023-01-31 14:01:47 -0800

    Revert "Add IBroker implementation for MSALRuntime (#563)"
    This reverts commit aacb4395227dff6d3aef3223889d5e1e56323371.

--- msal4j-sdk/src/main/java/com/microsoft/aad/msal4j/IBroker.java
+++ msal4j-sdk/src/main/java/com/microsoft/aad/msal4j/IBroker.java
@@ -3,80 +3,58 @@
 
 package com.microsoft.aad.msal4j;
 
-import com.nimbusds.jwt.JWTParser;
-
-import java.net.URL;
+import java.util.Set;
 import java.util.concurrent.CompletableFuture;
 
 /**
  * Used to define the basic set of methods that all Brokers must implement
  *
- * All methods are marked as default so they can be referenced by MSAL Java without an implementation,
- *  and most will simply throw an exception if not overridden by an IBroker implementation
+ * All methods are so they can be referenced by MSAL Java without an implementation, and by default simply throw an
+ * exception saying that a broker implementation is missing
  */
 public interface IBroker {
 
+    /**
+     * checks if a IBroker implementation exists
+     */
+
+    default boolean isAvailable(){
+        return false;
+    }
     /**
      * Acquire a token silently, i.e. without direct user interaction
      *
      * This may be accomplished by returning tokens from a token cache, using cached refresh tokens to get new tokens,
      * or via any authentication flow where a user is not prompted to enter credentials
+     *
+     * @param requestParameters MsalRequest object which contains everything needed for the broker implementation to make a request
+     * @return IBroker implementations will return an AuthenticationResult object
      */
-    default CompletableFuture<IAuthenticationResult> acquireToken(PublicClientApplication application, SilentParameters requestParameters) {
+    default IAuthenticationResult acquireToken(PublicClientApplication application, SilentParameters requestParameters) {
         throw new MsalClientException("Broker implementation missing", AuthenticationErrorCode.MISSING_BROKER);
     }
 
     /**
      * Acquire a token interactively, by prompting users to enter their credentials in some way
+     *
+     * @param requestParameters MsalRequest object which contains everything needed for the broker implementation to make a request
+     * @return IBroker implementations will return an AuthenticationResult object
      */
-    default CompletableFuture<IAuthenticationResult> acquireToken(PublicClientApplication application, InteractiveRequestParameters parameters) {
+    default IAuthenticationResult acquireToken(PublicClientApplication application, InteractiveRequestParameters requestParameters) {
         throw new MsalClientException("Broker implementation missing", AuthenticationErrorCode.MISSING_BROKER);
     }
 
     /**
      * Acquire a token silently, i.e. without direct user interaction, using username/password authentication
+     *
+     * @param requestParameters MsalRequest object which contains everything needed for the broker implementation to make a request
+     * @return IBroker implementations will return an AuthenticationResult object
      */
-    default CompletableFuture<IAuthenticationResult> acquireToken(PublicClientApplication application, UserNamePasswordParameters parameters) {
+    default IAuthenticationResult acquireToken(PublicClientApplication application, UserNamePasswordParameters requestParameters) {
         throw new MsalClientException("Broker implementation missing", AuthenticationErrorCode.MISSING_BROKER);
     }
 
-    default void removeAccount(PublicClientApplication application, IAccount account) throws MsalClientException {
+    default CompletableFuture removeAccount(IAccount account) {
         throw new MsalClientException("Broker implementation missing", AuthenticationErrorCode.MISSING_BROKER);
-    }
-
-    default boolean isBrokerAvailable() {
-        throw new MsalClientException("Broker implementation missing", AuthenticationErrorCode.MISSING_BROKER);
-    }
-
-    /**
-     * MSAL Java's AuthenticationResult requires several package-private classes that a broker implementation can't access,
-     *  so this helper method can be used to create AuthenticationResults from within the MSAL Java package
-     */
-    default IAuthenticationResult parseBrokerAuthResult(String authority, String idToken, String accessToken,
-                                                            String accountId, String clientInfo,
-                                                            long accessTokenExpirationTime) {
-
-        AuthenticationResult.AuthenticationResultBuilder builder =  AuthenticationResult.builder();
-
-        try {
-            if (idToken != null) {
-                builder.idToken(idToken);
-                if (accountId!= null) {
-                    String idTokenJson =
-                            JWTParser.parse(idToken).getParsedParts()[1].decodeToString();
-                    //TODO: need to figure out if 'policy' field is relevant for brokers
-                    builder.accountCacheEntity(AccountCacheEntity.create(clientInfo,
-                            Authority.createAuthority(new URL(authority)), JsonHelper.convertJsonToObject(idTokenJson,
-                                    IdToken.class), null));
-                }
-            }
-            if (accessToken != null) {
-                builder.accessToken(accessToken);
-                builder.expiresOn(accessTokenExpirationTime);
-            }
-        } catch (Exception e) {
-            throw new MsalClientException(String.format("Exception when converting broker result to MSAL Java AuthenticationResult: %s", e.getMessage()), AuthenticationErrorCode.MSALJAVA_BROKERS_ERROR);
-        }
-        return builder.build();
     }
 }
