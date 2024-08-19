51a0101f27256d03e1c82f8c9c29a26cc7b6b730
avdunn <avdunn@microsoft.com>
2024-08-19 16:24:43 -0700

    Add object ID option for Managed Identity

--- msal4j-sdk/src/main/java/com/microsoft/aad/msal4j/ManagedIdentityRequest.java
+++ msal4j-sdk/src/main/java/com/microsoft/aad/msal4j/ManagedIdentityRequest.java
@@ -4,15 +4,20 @@
 package com.microsoft.aad.msal4j;
 
 import com.nimbusds.oauth2.sdk.util.URLUtils;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import java.net.MalformedURLException;
 import java.net.URI;
 import java.net.URISyntaxException;
 import java.net.URL;
+import java.util.Collections;
 import java.util.List;
 import java.util.Map;
 
 class ManagedIdentityRequest extends MsalRequest {
+
+    private static final Logger LOG = LoggerFactory.getLogger(ManagedIdentityRequest.class);
 
     URI baseEndpoint;
 
@@ -52,5 +57,22 @@ class ManagedIdentityRequest extends MsalRequest {
         String queryString = URLUtils.serializeParameters(queryParameters);
 
         return baseEndpoint.toString() + "?" + queryString;
+    }
+
+    void addUserAssignedIdToQuery(ManagedIdentityIdType idType, String userAssignedId) {
+        switch (idType) {
+            case CLIENT_ID:
+                LOG.info("[Managed Identity] Adding user assigned client id to the request.");
+                queryParameters.put(Constants.MANAGED_IDENTITY_CLIENT_ID, Collections.singletonList(userAssignedId));
+                break;
+            case RESOURCE_ID:
+                LOG.info("[Managed Identity] Adding user assigned resource id to the request.");
+                queryParameters.put(Constants.MANAGED_IDENTITY_RESOURCE_ID, Collections.singletonList(userAssignedId));
+                break;
+            case OBJECT_ID:
+                LOG.info("[Managed Identity] Adding user assigned object id to the request.");
+                queryParameters.put(Constants.MANAGED_IDENTITY_OBJECT_ID, Collections.singletonList(userAssignedId));
+                break;
+        }
     }
 }
